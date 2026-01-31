7784dfe36333e7e9ca8c827632c209617cd03e88
grusu94 <georgerusu994@gmail.com>
2026-01-31 12:24:12 +0200

    Initial commit

---
+++ src/main/java/com/github/grusu94/gh_simple_spring/GhSimpleSpringApplication.java
@@ -0,0 +1,13 @@
+package com.github.grusu94.gh_simple_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhSimpleSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhSimpleSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/grusu94/gh_simple_spring/GhSimpleSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.grusu94.gh_simple_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhSimpleSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
