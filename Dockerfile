7459649f73567e8938153c13ce99c5b8c038df58
grusu94 <georgerusu994@gmail.com>
2026-03-07 16:43:42 +0200

    Add Dockerfile and build workflow

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+# -------- BUILD STAGE --------
+FROM maven:3.9.6-eclipse-temurin-17 AS build
+WORKDIR /app
+COPY. .
+RUN mvn clean package -DskipTests
+
+# -------- PACKAGE STAGE --------
+FROM eclipse-temurin:21-jdk-jammy
+WORKDIR /app
+COPY --from=build /app/target/*.jar gh-simple-spring.jar
+EXPOSE 9000
+ENTRYPOINT ["java","-jar","gh-simple-spring.jar"]
---
+++ src/main/java/com/github/grusu94/gh_simple_spring/controller/SimpleController.java
@@ -0,0 +1,16 @@
+package com.github.grusu94.gh_simple_spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/")
+public class SimpleController {
+
+    @GetMapping(value = "/users/{userId}")
+    public String greetingsFrom (@PathVariable Integer userId) {
+        return "Hello from userId " + userId;
+    }
+}
