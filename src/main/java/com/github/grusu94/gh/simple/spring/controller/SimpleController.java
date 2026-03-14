67144727dccf8b1aefad1b733aec0d97dabb8580
grusu94 <georgerusu994@gmail.com>
2026-03-14 10:17:59 +0200

    Add config server

--- src/main/java/com/github/grusu94/gh/simple/spring/controller/SimpleController.java
+++ src/main/java/com/github/grusu94/gh/simple/spring/controller/SimpleController.java
@@ -1,5 +1,6 @@
 package com.github.grusu94.gh.simple.spring.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/")
 public class SimpleController {
 
+    @Value("${simple.greeting.message}")
+    private String greetingMessage;
+
+
     @GetMapping(value = "/users/{userId}")
     public String greetingsFrom (@PathVariable Integer userId) {
-        return "Hello from userId " + userId;
+        return greetingMessage +  " userId " + userId;
     }
 }
--- src/test/java/com/github/grusu94/gh/simple/spring/GhSimpleSpringApplicationTests.java
+++ src/test/java/com/github/grusu94/gh/simple/spring/GhSimpleSpringApplicationTests.java
@@ -3,7 +3,10 @@ package com.github.grusu94.gh.simple.spring;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.cloud.config.enabled=false",
+        "spring.profiles.active=test"
+})
 class GhSimpleSpringApplicationTests {
 
 	@Test
